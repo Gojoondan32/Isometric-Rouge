171109e1ebec1ea7ebcfa484bc0f76669c4e179a
CodeSmither <the.space.radish@gmail.com>
2024-10-13 18:21:15 +0100

    Grid System 0,0,0 update
    Generate middle of grid at 0,0,0

--- Isometric-Rouge-Game/Assets/Scripts/MapGenerationScript.cs
+++ Isometric-Rouge-Game/Assets/Scripts/MapGenerationScript.cs
@@ -6,18 +6,18 @@ public class MapGenerationScript : MonoBehaviour
 {
     public GameObject gridObject;
 
-    private int worldSizeX = 10;
+    private int worldSizeX = 100;
 
-    private int worldSizeZ = 10;
+    private int worldSizeZ = 100;
 
-    private int gridOffset = 2;
+    private int gridOffset = 10;
 
     private void Start()
     {
         for(int x = 0; x < worldSizeX; x++ ) {
             for( int z = 0; z < worldSizeZ; z++) {
 
-                Vector3 pos = new Vector3(x * gridOffset, 0, z * gridOffset);
+                Vector3 pos = new Vector3((x - (worldSizeX/2)) * gridOffset , 0, (z - (worldSizeZ/2)) * gridOffset);
                 GameObject block = Instantiate(gridObject, pos, Quaternion.identity) as GameObject;
 
                 block.transform.SetParent(this.transform);
