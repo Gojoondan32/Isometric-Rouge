7b97763d68017f2c6c6f00ee0ec63575fc384769
CodeSmither <the.space.radish@gmail.com>
2024-10-13 18:41:10 +0100

    Standard Colour Generation
    Generates Coloured Grid in a predictable Repeated Pattern

--- Isometric-Rouge-Game/Assets/Scripts/MapGenerationScript.cs
+++ Isometric-Rouge-Game/Assets/Scripts/MapGenerationScript.cs
@@ -4,23 +4,28 @@ using UnityEngine;
 
 public class MapGenerationScript : MonoBehaviour
 {
-    public GameObject gridObject;
+    public GameObject[] gridObjects;
 
     private int worldSizeX = 100;
 
     private int worldSizeZ = 100;
 
-    private int gridOffset = 10;
+    private int gridOffset = 20;
 
     private void Start()
     {
-        for(int x = 0; x < worldSizeX; x++ ) {
+        int y = 0;
+        for (int x = 0; x < worldSizeX; x++ ) {
             for( int z = 0; z < worldSizeZ; z++) {
 
                 Vector3 pos = new Vector3((x - (worldSizeX/2)) * gridOffset , 0, (z - (worldSizeZ/2)) * gridOffset);
-                GameObject block = Instantiate(gridObject, pos, Quaternion.identity) as GameObject;
-
+                GameObject block = Instantiate(gridObjects[y], pos, Quaternion.identity) as GameObject;
+                y++;
                 block.transform.SetParent(this.transform);
+                if(y == 3)
+                {
+                    y = 0;
+                }
             }
         }
     }
