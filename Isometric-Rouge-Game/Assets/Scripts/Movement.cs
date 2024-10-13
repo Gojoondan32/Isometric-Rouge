9c8bd303627c422dc15a24bc013cfbddf6667a99
Gojoondan32 <62604263+Gojoondan32@users.noreply.github.com>
2024-10-13 18:00:45 +0100

    Created input actions

---
+++ Isometric-Rouge-Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private PlayerInput _playerInput;
+    
+    private void OnEnable() {
+        
+    }
+}
