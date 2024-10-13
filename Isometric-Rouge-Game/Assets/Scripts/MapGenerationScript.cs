8a02aee2f2febe1f1b7a818e070bc0858ca2186a
CodeSmither <the.space.radish@gmail.com>
2024-10-13 19:42:52 +0100

    Basic Generation Behaviour
    Has the ability to Generate a start and finish as well as full and empty squares based on randomly generated parameters.

--- Isometric-Rouge-Game/Assets/Scripts/MapGenerationScript.cs
+++ Isometric-Rouge-Game/Assets/Scripts/MapGenerationScript.cs
@@ -6,27 +6,72 @@ public class MapGenerationScript : MonoBehaviour
 {
     public GameObject[] gridObjects;
 
-    private int worldSizeX = 100;
+    private int worldSizeX = 10;
 
-    private int worldSizeZ = 100;
+    private int worldSizeZ = 10;
 
     private int gridOffset = 20;
 
+    [SerializeField]
+    private int start = 0;
+    [SerializeField]
+    private int end = 0;
+
+    private int CurrentChunk = 0;
+
     private void Start()
     {
-        int y = 0;
-        for (int x = 0; x < worldSizeX; x++ ) {
-            for( int z = 0; z < worldSizeZ; z++) {
+        CalcStartandEnd();
+        GenerateLevel();
+    }
 
-                Vector3 pos = new Vector3((x - (worldSizeX/2)) * gridOffset , 0, (z - (worldSizeZ/2)) * gridOffset);
-                GameObject block = Instantiate(gridObjects[y], pos, Quaternion.identity) as GameObject;
-                y++;
+    private void GenerateLevel()
+    {
+        for (int x = 0; x < worldSizeX; x++)
+        {
+            for (int z = 0; z < worldSizeZ; z++)
+            {
+                Vector3 pos = new Vector3((x - (worldSizeX / 2)) * gridOffset, 0, (z - (worldSizeZ / 2)) * gridOffset);
+                GameObject block = Instantiate(gridObjects[SelectRoomType()], pos, Quaternion.identity) as GameObject;
                 block.transform.SetParent(this.transform);
-                if(y == 3)
-                {
-                    y = 0;
-                }
+
+                CurrentChunk++;
             }
         }
+    }
+    private void CalcStartandEnd()
+    {
+        while (start == end)
+        {
+            start = Random.Range(0, 101);
+
+            end = Random.Range(0, 101);
+        }
+        GenerateLevel();
+    }
+
+    private int SelectRoomType()
+    {
+        int GridObjType;
+        if (CurrentChunk == start)
+        {
+            GridObjType = 1;
+        }
+        else if (CurrentChunk == end)
+        {
+            GridObjType = 2;
+        }
+        else {
+            int Choice = Random.Range(0, 2);
+            if (Choice == 0)
+            {
+                GridObjType = 0;
+            }
+            else
+            {
+                GridObjType = 3;
+            }
+        }
+        return GridObjType;
     }
 }
