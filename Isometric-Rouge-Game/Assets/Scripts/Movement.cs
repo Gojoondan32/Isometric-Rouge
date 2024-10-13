fc8eb0b4c6d6bd77ca8188f0ce6913d7e6b2d01d
Gojoondan32 <62604263+Gojoondan32@users.noreply.github.com>
2024-10-13 20:03:08 +0100

    Started working on movement and rotation
    Movement is done but the rotation is being a pain

--- Isometric-Rouge-Game/Assets/Scripts/Movement.cs
+++ Isometric-Rouge-Game/Assets/Scripts/Movement.cs
@@ -1,13 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private PlayerInput _playerInput;
-    
-    private void OnEnable() {
+    [SerializeField] private CharacterController _controller;
+    [SerializeField] private float _speed;
+    private Vector2 inputVector;
+
+    private void OnEnable(){
+        _playerInput.onActionTriggered += OnActionTriggered;
+    }
+
+    private void Update(){
+        _controller.Move(new Vector3(inputVector.x, 0, inputVector.y) * _speed * Time.deltaTime);
+        Rotate();
+    }
+
+    private void OnActionTriggered(InputAction.CallbackContext context){
+        OnMove(context);
+    }
+
+    public void OnMove(InputAction.CallbackContext context){
+        
+        inputVector = context.ReadValue<Vector2>();
+    }
+
+
+    public Transform temp;
+    private void Rotate(){
+        Vector3 mousePos = new Vector3(Mouse.current.position.ReadValue().x, 0, Mouse.current.position.ReadValue().y);
+        Ray mouseWorldPos = Camera.main.ScreenPointToRay(mousePos);
+        //Vector3 dir = mouseWorldPos - Camera.main.transform.position;
+        RaycastHit hit;
+        Debug.Log("Running");
+        if(Physics.Raycast(mouseWorldPos, Mathf.Infinity)){
+            Debug.Log("hitting");
+            //Debug.DrawRay(Camera.main.transform.position, mouseWorldPos., Color.red, 1f);
+            //temp.position = hit.point;
+        }
         
     }
+
 }
