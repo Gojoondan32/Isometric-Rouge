300f4a308331d9a7a146164c96809a0aa09efb18
Gojoondan32 <62604263+Gojoondan32@users.noreply.github.com>
2024-10-18 21:29:36 +0100

    Fixed rotation (Kinda)

--- Isometric-Rouge-Game/Assets/Scripts/Movement.cs
+++ Isometric-Rouge-Game/Assets/Scripts/Movement.cs
@@ -1,15 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
 
 public class Movement : MonoBehaviour
 {
     [SerializeField] private PlayerInput _playerInput;
     [SerializeField] private CharacterController _controller;
     [SerializeField] private float _speed;
+    [SerializeField] private float _rotationSpeed;
     private Vector2 inputVector;
 
     private void OnEnable(){
@@ -17,8 +22,11 @@ public class Movement : MonoBehaviour
     }
 
     private void Update(){
-        _controller.Move(new Vector3(inputVector.x, 0, inputVector.y) * _speed * Time.deltaTime);
         Rotate();
+
+        if(inputVector.y <= 0) return;
+        //_controller.Move(new Vector3(inputVector.x, 0, inputVector.y) * _speed * Time.deltaTime);
+        _controller.Move(transform.forward * _speed * Time.deltaTime);
     }
 
     private void OnActionTriggered(InputAction.CallbackContext context){
@@ -33,17 +41,15 @@ public class Movement : MonoBehaviour
 
     public Transform temp;
     private void Rotate(){
-        Vector3 mousePos = new Vector3(Mouse.current.position.ReadValue().x, 0, Mouse.current.position.ReadValue().y);
-        Ray mouseWorldPos = Camera.main.ScreenPointToRay(mousePos);
-        //Vector3 dir = mouseWorldPos - Camera.main.transform.position;
-        RaycastHit hit;
-        Debug.Log("Running");
-        if(Physics.Raycast(mouseWorldPos, Mathf.Infinity)){
-            Debug.Log("hitting");
-            //Debug.DrawRay(Camera.main.transform.position, mouseWorldPos., Color.red, 1f);
-            //temp.position = hit.point;
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+        if(Physics.Raycast(ray, out RaycastHit hit, 100f, LayerMask.GetMask("Ground"))){
+            temp.position = hit.point;
+            Vector3 dir = hit.point - transform.position.normalized;
+            float angle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
+            Quaternion angleAxis = Quaternion.AngleAxis(angle, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, angleAxis, Time.deltaTime * _rotationSpeed);
         }
-        
     }
 
 }
